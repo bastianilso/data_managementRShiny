d3df3eea403b5d11bd5400bdd60e4a6d95060514
Bastian Ilso <hougaard.junior@gmail.com>
2021-03-12 14:47:29 +0100

    data_selection_summary: update to fit new format
    We should now expect timestamp columns to be named
    Timestamp.Meta, rather than MetaTimestamp.

--- data_management/modules/data_selection_summary_module.R
+++ data_management/modules/data_selection_summary_module.R
@@ -11,11 +11,11 @@ data_selection_summary <- function(input, output, session, df) {
   output$data_selection_text <- renderUI({
     HTML(paste(
       "Data from ",
-      wday(min(as.character(df()$MetaTimestamp), na.rm=T), abbr = F, label=T),
-      format(date(min(as.character(df()$MetaTimestamp), na.rm=T)), "%d %b %Y"),
-      sprintf("%02d:%02d", hour(min(as.character(df()$MetaTimestamp), na.rm=T)), minute(min(as.character(df()$MetaTimestamp), na.rm=T))),
+      wday(min(as.character(df()$Timestamp.Meta), na.rm=T), abbr = F, label=T),
+      format(date(min(as.character(df()$Timestamp.Meta), na.rm=T)), "%d %b %Y"),
+      sprintf("%02d:%02d", hour(min(as.character(df()$Timestamp.Meta), na.rm=T)), minute(min(as.character(df()$Timestamp.Meta), na.rm=T))),
       "<br><small>",
-      as.character(min(as.character(df()$MetaEmail), na.rm=T)),
+      as.character(min(as.character(df()$Email.Meta), na.rm=T)),
       "(",
       str_sub(max(as.character(df()$SessionID), na.rm=T),-6,-1),
       ")",
